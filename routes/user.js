d2f9e9c217637a65eb01327fd4f14f64e2fc32d8
Christopher Young <christopheryoung@MacBook123.local>
2023-03-17 11:13:11 -0700

    updated user routes

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,18 @@ const bcrypt = require("bcryptjs");
 // Models
 const User = require("../models/userModel");
 
-userRoutes.route("/users")
+userRoutes.route("/user")
+//----- Retrieve all users
+.get((req, res) => {
+  User.find({})
+  .then(allDocs => {
+    res.json({
+      success: true,
+      users: allDocs
+    })
+  })
+  .catch(err => console.log(err));
+})
 //----- Create new user
 .post((req, res) => {
   // Encrypt password
@@ -31,6 +42,30 @@ userRoutes.route("/users")
       });
     });
   });
+});
+
+userRoutes.route("/user/:id")
+//----- Retrieve given user
+.get((req, res) => {
+  User.findById(req.params.id)
+  .then(doc => {
+    res.json({
+      success: true,
+      user: doc
+    });
+  })
+  .catch(err => console.log(err));
+})
+//----- Delete given user
+.delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+  .then(deletedDoc => {
+    res.json({
+      success: true,
+      user: deletedDoc
+    })
+  })
+  .catch(err => console.log(err));
 });
 
 module.exports = userRoutes;
